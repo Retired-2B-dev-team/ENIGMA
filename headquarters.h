f61820325ebc7d01507c1ec898017a288f17366c
Antek <107063507+JENOT-ANT@users.noreply.github.com>
2022-06-21 19:10:21 +0200

    Create headquarters.h

---
+++ headquarters.h
@@ -0,0 +1,27 @@
+// Library with some general purpose functions.
+// Copyright: (!c) Public Domain | Author: <JENOT>
+
+namespace hq
+{
+
+	// useful / "magic" numbers:
+	const int ASCII_CHARS_AMOUNT = 256;
+
+
+	void clear_int_array(int* array, int beginning, int end, int empty_number = 0)
+	{
+		for (int i = beginning; i < end; i++)
+		{
+			array[i] = empty_number;
+		}
+	}
+
+	void clear_char_array(char* array, int beginning, int end, char empty_char = (char)0)
+	{
+		for (int i = beginning; i < end; i++)
+		{
+			array[i] = empty_char;
+		}
+	}
+
+}
