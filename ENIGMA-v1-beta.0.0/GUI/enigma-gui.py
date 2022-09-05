d3fc1c7b52d6ccd0570a37dd7997e0acac1a8574
Antek <antekswitaj@outlook.com>
2022-09-05 21:19:35 +0200

    Update GUI

--- ENIGMA-v1-beta.0.0/GUI/enigma-gui.py
+++ ENIGMA-v1-beta.0.0/GUI/enigma-gui.py
@@ -3,6 +3,7 @@ from os import chdir
 from os.path import dirname
 
 from tkinter import *
+from tkinter.scrolledtext import ScrolledText
 from tkinter.font import BOLD
 from tkinter.ttk import Separator
 
@@ -13,11 +14,12 @@ INPUT_PATH: str = "GUI/input.txt"
 OUTPUT_PATH: str = "GUI/output.txt"
 
 
-def encrypt(text: str, output: Label):
-    text = text.removesuffix('\n')
+def encrypt(input_box: Text, output: Text):
+    inp = input_box.get("1.0", "end")
+    inp = inp.removesuffix('\n').upper()
 
     input_file = open(INPUT_PATH, "w")
-    input_file.write(text)
+    input_file.write(inp)
     input_file.close()
     
     system(f".\\{EXE_NAME} -f {INPUT_PATH} > {OUTPUT_PATH}")
@@ -25,8 +27,24 @@ def encrypt(text: str, output: Label):
     output_file = open(OUTPUT_PATH, "r")
     text = output_file.read()
     output_file.close()
+    
+    if '\n' in inp:
+        inp = inp.replace('\n', " \\n ")
+        text = text.removesuffix('\n').replace('\n', "\n\t")
+        text = f"[\"{inp}\"]:\n\t{text}\n"
+    else:
+        text = f"[\"{inp}\"]> {text}"
 
-    output.config(text=text)
+    input_box.delete("1.0", "end")
+    
+    output.config(state="normal")
+    output.insert("end", text)
+    output.config(state="disabled")
+
+def clear(output: Text):
+    output.config(state="normal")
+    output.delete("1.0", "end")
+    output.config(state="disabled")
 
 def main():
     chdir(dirname(__file__))
@@ -38,19 +56,21 @@ def main():
     frame = Frame(window)
     frame.grid()
 
-    Label(frame, text="output:\n\n\n").grid(row=0, column=0)
+    Label(frame, text="Output:", font="BOLD").grid(row=0, column=0)
     
-    output = Label(frame, font=BOLD, fg="green", justify="left")
+    output = ScrolledText(frame, width=60, height=6, font=BOLD, fg="green", bg="lightgray", border="10", state="disabled", relief="flat")
     output.grid(row=0, column=1)
 
     Separator(frame, orient="horizontal").grid(row=1, column=0, sticky='nswe')
     Separator(frame, orient="horizontal").grid(row=1, column=1, sticky='nswe')
 
-    input_box = Text(frame, width=50, height=2)
+    encrypt_button = Button(frame, text="-Encrpt-", command=lambda: encrypt(input_box, output), font="BOLD")
+    encrypt_button.grid(row=2, column=0)
+
+    input_box = ScrolledText(frame, width=60, height=3, font="BOLD")
     input_box.grid(row=2, column=1)
-    
-    encrypt_button = Button(frame, text="-Encrpt-", command=lambda: encrypt(input_box.get("1.0", "end"), output))
-    encrypt_button.grid(row=3, column=1)
+
+    Button(frame, text="-Clear-", command=lambda: clear(output), font="BOLD").grid(row=3, column=1)
 
     window.mainloop()
 
