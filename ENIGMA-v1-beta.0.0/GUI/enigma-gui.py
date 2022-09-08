84f7767f82fed8c4766e4df2230416e5d0cf6456
Antek <antekswitaj@outlook.com>
2022-09-08 22:16:59 +0200

    More legible form of output

--- ENIGMA-v1-beta.0.0/GUI/enigma-gui.py
+++ ENIGMA-v1-beta.0.0/GUI/enigma-gui.py
@@ -31,9 +31,9 @@ def encrypt(input_box: Text, output: Text):
     if '\n' in inp:
         inp = inp.replace('\n', " \\n ")
         text = text.removesuffix('\n').replace('\n', "\n\t")
-        text = f"[\"{inp}\"]:\n\t{text}\n"
+        text = f"~#~[\"{inp}\"]:\n\n\t{text}\n"
     else:
-        text = f"[\"{inp}\"]> {text}"
+        text = f"~#~[\"{inp}\"]> {text}"
 
     input_box.delete("1.0", "end")
     
@@ -47,6 +47,7 @@ def clear(output: Text):
     output.config(state="disabled")
 
 def main():
+    print(__file__)
     chdir(dirname(__file__))
     chdir("../")
 
@@ -58,7 +59,7 @@ def main():
 
     Label(frame, text="Output:", font="BOLD").grid(row=0, column=0)
     
-    output = ScrolledText(frame, width=60, height=6, font=BOLD, fg="green", bg="lightgray", border="10", state="disabled", relief="flat")
+    output = ScrolledText(frame, width=80, height=12, font=BOLD, fg="green", bg="lightgray", border="10", state="disabled", relief="flat")
     output.grid(row=0, column=1)
 
     Separator(frame, orient="horizontal").grid(row=1, column=0, sticky='nswe')
@@ -67,7 +68,7 @@ def main():
     encrypt_button = Button(frame, text="-Encrpt-", command=lambda: encrypt(input_box, output), font="BOLD")
     encrypt_button.grid(row=2, column=0)
 
-    input_box = ScrolledText(frame, width=60, height=3, font="BOLD")
+    input_box = ScrolledText(frame, width=80, height=6, font="BOLD")
     input_box.grid(row=2, column=1)
 
     Button(frame, text="-Clear-", command=lambda: clear(output), font="BOLD").grid(row=3, column=1)
